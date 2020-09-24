9efd7bc41a3b6ad9449069b6e4b8161703a2c0a1
ferozkhan <ferozkhan12123outlook.com>
2020-09-24 20:29:24 +0500

    make it better

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 3000; //Server Port
+const { v4: uuidV4 } = require("uuid"); // This Use For Random URL
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//Routes
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("room", { roomId: req.params.room });
+});
+
+//web socket
+io.on("connection" ,(socket) => {
+    socket.on("join-room", (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId)
+    });
+
+    socket.on("disconnect", (roomId, userId) => {
+        socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+});
+
+//Server
+server.listen(port, () => {
+    console.log(`Server Running at Port : ${port}`);
+});
